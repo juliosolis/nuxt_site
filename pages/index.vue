a9753d85e4547aebc614a7b4d0216f7aaaf88d4f
Julio Solis <js@juliosolis.com>
2017-11-05 21:43:55 -0600

    Nuxt static website

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Julio Solis</h1>
+      <h2 class="subtitle">a Software Developer based in Managua, Nicaragua.</h2>
+    </div>
+  </section>
+</template>
